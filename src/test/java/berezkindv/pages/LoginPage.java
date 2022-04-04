ead3791f0b47c8bde00f7c99aceffb8e0d839224
DaymianDark <nightkonn@gmail.com>
2022-04-04 15:18:58 +0300

    login test selectors changed

--- src/test/java/berezkindv/pages/LoginPage.java
+++ src/test/java/berezkindv/pages/LoginPage.java
@@ -16,19 +16,17 @@ public class LoginPage {
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     private static final SelenideElement loginButton = $(".header").$(byText("Войти")),
-            loginFormCheck = $("div[id='view_container']").$(byText("Забыли адрес электронной почты?")),
+//            loginFormCheck = $("div[id='view_container']").$(byText("Забыли адрес электронной почты?")),
+            loginFormCheck = $x("//button[text() = 'Забыли адрес электронной почты?']"),
             emailInput = $("#identifierId"),
             emailInputNextButton = $("#identifierNext").$(byText("Далее")),
-            passwordFormCheck = $("div[id='view_container']").$(byText("Забыли пароль?")),
+//            passwordFormCheck = $("div[id='view_container']").$(byText("Забыли пароль?")),
+            passwordFormCheck = $x("//button[text() = 'Забыли адрес электронной почты?']"),
             passwordInput = $("input[type='password']"),
             passwordInputNextButton = $("#passwordNext").$(byText("Далее")),
             loggedIcon = $("img[class='gb_Aa gbii']"),
-//            loggedTooltip = $("body > div:nth-child(30)");
             loggedTooltip = $x("//div[text() = 'John Smith']");
 
-    // body > div.gb_Qe
-    // body > div.gb_Qe > div > div:nth-child(3)
-    // /html/body/div[17]/div/div[2]
 
     @Step("Открываем страницу 'https://www.google.com/intl/ru/gmail/about/'")
     public LoginPage openPage() {
@@ -81,7 +79,6 @@ public class LoginPage {
     @Step("Проверяем логин")
     public LoginPage loggedCheck() {
         loggedIcon.hover();
-//        loggedTooltip.shouldHave(text("John Smith"));
         loggedTooltip.should(exist);
         return this;
     }
