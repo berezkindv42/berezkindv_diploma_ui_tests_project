49df233979673183117fa4e70619f33e4b6f9310
DaymianDark <nightkonn@gmail.com>
2022-04-04 21:04:00 +0300

    driver setting changed

--- src/test/java/berezkindv/helpers/DriverSettings.java
+++ src/test/java/berezkindv/helpers/DriverSettings.java
@@ -23,13 +23,13 @@ public class DriverSettings {
         chromeOptions.addArguments("--disable-infobars");
         chromeOptions.addArguments("--disable-popup-blocking");
         chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addArguments("--lang=en-us");
+//        chromeOptions.addArguments("--lang=en-en");
 
-        HashMap<String, String> settings = new HashMap<String, String>();
-        settings.put("intl.accept_languages", "en-US");
-        chromeOptions.setExperimentalOption("prefs", settings);
+//        HashMap<String, String> settings = new HashMap<String, String>();
+//        settings.put("intl.accept_languages", "en-US");
+//        chromeOptions.setExperimentalOption("prefs", settings);
 
-//        chromeOptions.addArguments("intl.accept_languages", "ru-RU");
+//        chromeOptions.addPreference("intl.accept_languages", "en-US");
 
         if (Project.isWebMobile()) { // for chrome only
             Map<String, Object> mobileDevice = new HashMap<>();
