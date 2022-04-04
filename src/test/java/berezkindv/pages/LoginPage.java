6c5dc350895c6a91449c1b1f4035c02f752cab45
DaymianDark <nightkonn@gmail.com>
2022-04-04 21:21:35 +0300

    login test changed

--- src/test/java/berezkindv/pages/LoginPage.java
+++ src/test/java/berezkindv/pages/LoginPage.java
@@ -48,7 +48,7 @@ public class LoginPage {
 
     @Step("Нажимаем кнопку выбора языка")
     public LoginPage selectRuLanguage() {
-        languageSelect.scrollTo().click();
+        languageSelect.scrollIntoView(true).click();
         return this;
     }
 
