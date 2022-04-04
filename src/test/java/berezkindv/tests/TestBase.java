d99db02095afa629ebb179a6d65536ac7338e54e
DaymianDark <nightkonn@gmail.com>
2022-04-04 22:53:19 +0300

    BeforeEach method added

--- src/test/java/berezkindv/tests/TestBase.java
+++ src/test/java/berezkindv/tests/TestBase.java
@@ -1,16 +1,25 @@
 package berezkindv.tests;
 
 import berezkindv.config.Project;
+import berezkindv.config.ProjectConfig;
 import berezkindv.helpers.AllureAttachments;
 import berezkindv.helpers.DriverSettings;
 import berezkindv.helpers.DriverUtils;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 @ExtendWith({AllureJunit5.class})
@@ -21,6 +30,14 @@ public class TestBase {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DriverSettings.configure();
     }
+
+    @BeforeEach
+    static void setUpIch() throws MalformedURLException {
+        if (Project.config.remoteDriverUrl() == null) return;
+        WebDriver driver = new RemoteWebDriver(new URL(Project.config.remoteDriverUrl()), Configuration.browserCapabilities);
+        WebDriverRunner.setWebDriver(driver);
+    }
+
 
     @AfterEach
     public void addAttachments() {
