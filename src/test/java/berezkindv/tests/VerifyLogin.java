e183fd175206cd58a574cebda8f51310fb0ade30
DaymianDark <nightkonn@gmail.com>
2022-04-06 16:36:13 +0300

    verify test changed

--- src/test/java/berezkindv/tests/VerifyLogin.java
+++ src/test/java/berezkindv/tests/VerifyLogin.java
@@ -1,27 +1,32 @@
 package berezkindv.tests;
 
+import berezkindv.config.CredentialsConfig;
 import berezkindv.pages.LoginPage;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class VerifyLogin extends TestBase {
 
     LoginPage loginPage = new LoginPage();
+    CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     // Andere Option wählen
 
     @Test
     @Tag("verify")
     public void verifyLoginTest() {
-        loginPage
-                .openAccountPage()
-                .typeEmail()
-                .pushEmailNextButton()
-                .typePassword()
-                .pushPasswordNextButton();
+
+        open("https://accounts.google.com/ServiceLogin?hl=ru");
+        $("#identifierId").setValue(config.userEmail());
+        $("#identifierNext").$(byText("Далее")).click();
+        $("input[type='password']").setValue(config.userPassword());
+        $("#passwordNext").$(byText("Далее"));
         $x("//div[text() = 'Andere Option wählen']").click();
-        loginPage.loggedCheck();
+        loginPage
+                .loggedCheck();
     }
 }
