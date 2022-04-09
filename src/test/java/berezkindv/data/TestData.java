9ca0d346bf3cd29602cab4311b3ac45a21ed665d
DaymianDark <nightkonn@gmail.com>
2022-04-09 12:17:34 +0300

    clean project

--- src/test/java/berezkindv/data/TestData.java
+++ src/test/java/berezkindv/data/TestData.java
@@ -2,7 +2,6 @@ package berezkindv.data;
 
 public class TestData {
 
-    public static String gmailAccountPage = "https://accounts.google.com/ServiceLogin?hl=ru";
     public static String gmailPage = "https://www.google.com/intl/ru/gmail/about/?hl=ru";
     public static String testMailSubject = "Test mail " + System.currentTimeMillis();
     public static String testMailMessage = "This is a test message";
